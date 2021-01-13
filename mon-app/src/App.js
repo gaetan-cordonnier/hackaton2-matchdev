d5b25f4a74c23aa5737c8b1881c16b5e625b1c9f
Amelie Macquet <amelie.macquet.am@gmail.com>
2021-01-13 18:35:46 +0100

    creation component s'inscrire with form

--- mon-app/src/App.js
+++ mon-app/src/App.js
@@ -2,6 +2,7 @@ import Connection from './components/connection/index';
 import { Reset } from 'styled-reset';
 import Header from "./components/header/";
 import SeConnecter from "./components/seconnecter/index";  
+import Sinscrire from "./components/sinscrire/index";
 import './App.css';
 
 function App() {
