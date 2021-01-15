83d69532c9cdf6c5b463a90b5ca90b48ebfacda7
gaetan-cordonnier <gaetan.cordonnier@gmail.com>
2021-01-15 09:26:57 +0100

    Cors installation

--- back-end/server.js
+++ back-end/server.js
@@ -2,16 +2,18 @@ const express = require("express");
 const Connection = require("mysql2");
 const app = express();
 const { db } = require("./config");
+const cors = require("cors");
+app.use(cors());
 
 app.use(express.json());
 
 connection.connect(function (err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
+	if (err) {
+		console.error("error connecting: " + err.stack);
+		return;
+	}
 
-  console.log("connected as id " + connection.threadId);
+	console.log("connected as id " + connection.threadId);
 });
 
 /*app.get("/:id", (req, res) => {
@@ -33,82 +35,82 @@ connection.connect(function (err) {
 });*/
 
 app.listen(5050, () => {
-  console.log("Server is runing !");
+	console.log("Server is runing !");
 });
 
 //HomePage
 
 app.get("/", (req, res) => {
-  console.log("Home Page");
-  res.send("Welcome to Match♡Dev!");
+	console.log("Home Page");
+	res.send("Welcome to Match♡Dev!");
 });
 
 //Form
 
 app.post("/user", (req, res) => {
-  const {
-    prenom,
-    email,
-    password,
-    technos,
-    typeContrat,
-    codePostal,
-    anneesDeCode,
-    salaireVise,
-  } = req.body;
-  db.query(
-    "INSERT INTO candidats(prenom, nom, email, password, technos, technoVisee,typeContrat, anneesDeCode, salaireVise, codePostal) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
-    [
-      prenom,
-      "",
-      email,
-      password,
-      technos,
-      "",
-      typeContrat,
-      codePostal,
-      anneesDeCode,
-      salaireVise,
-    ],
-    (err, results) => {
-      if (err) {
-        res.status(500).send("Error saving a new user");
-      } else {
-        res.status(200).send("New user saved!");
-      }
-    }
-  );
+	const {
+		prenom,
+		email,
+		password,
+		technos,
+		typeContrat,
+		codePostal,
+		anneesDeCode,
+		salaireVise,
+	} = req.body;
+	db.query(
+		"INSERT INTO candidats(prenom, nom, email, password, technos, technoVisee,typeContrat, anneesDeCode, salaireVise, codePostal) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+		[
+			prenom,
+			"",
+			email,
+			password,
+			technos,
+			"",
+			typeContrat,
+			codePostal,
+			anneesDeCode,
+			salaireVise,
+		],
+		(err, results) => {
+			if (err) {
+				res.status(500).send("Error saving a new user");
+			} else {
+				res.status(200).send("New user saved!");
+			}
+		}
+	);
 });
 
 //Select Jobs
 
 app.get("/cards", (req, res) => {
-  db.query(
-    "SELECT a.idCand, a.idJob, c.idCompany, c.companyName, c.img, b.descriptionPreview, b.title, b.salary, a.distance, a.scoreNonSup FROM affinite a JOIN jobs b ON b.idJob = a.idJob JOIN company c ON c.idCompany = b.companyId WHERE a.idCand = 2 AND a.likes IS NULL ORDER BY a.scoreNonSup DESC LIMIT 10",
-    (err, results) => {
-      if (err) {
-        res.status(500).send("Error retrieving data");
+	db.query(
+		"SELECT a.idCand, a.idJob, c.idCompany, c.companyName, c.img, b.descriptionPreview, b.title, b.salary, a.distance, a.scoreNonSup FROM affinite a JOIN jobs b ON b.idJob = a.idJob JOIN company c ON c.idCompany = b.companyId WHERE a.idCand = 2 AND a.likes IS NULL ORDER BY a.scoreNonSup DESC LIMIT 10",
+		(err, results) => {
+			if (err) {
+				res.status(500).send("Error retrieving data");
 
-        return;
-      }
-      return res.status(200).json(results);
-    }
-  );
+				return;
+			}
+			return res.status(200).json(results);
+		}
+	);
 });
 
 //Check Profil
 
 app.get("/profil/:prenom", (req, res) => {
-  db.query(
-    "SELECT prenom, email, technos, typeContrat, codePostal, anneesDeCode from candidats WHERE prenom =?",
-    [req.params.prenom],
+	db.query(
+		"SELECT prenom, email, technos, typeContrat, codePostal, anneesDeCode from candidats WHERE prenom =?",
+		[req.params.prenom],
 
-    (err, results) => {
-      if (err) {
-        res.status(500).send("Error download information");
-      } else {
-        return res.status(200).json(results);
-      }
-    }
-  );
+		(err, results) => {
+			if (err) {
+				res.status(500).send("Error download information");
+			} else {
+				return res.status(200).json(results);
+			}
+		}
+	);
 });
