4ecc3c61a99f054287ff2c085840f6e578bffdac
Fx-Colliot <fxcolliot@gmail.com>
2021-01-15 11:00:58 +0100

    Delete connection.connect in server.js, change router profil whith /prenom, change css profil

--- mon-app/src/App.js
+++ mon-app/src/App.js
@@ -1,4 +1,3 @@
-
 import React from "react";
 import { Reset } from "styled-reset";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
@@ -11,24 +10,23 @@ import CardCompany from "./pages/cards/index";
 import Match from "./pages/match/index";
 import ProfilPage from "./pages/profil/index";
 
-
 import "./App.css";
 
 export default function App() {
-	return (
-		<div className="App">
-			<Reset />
-			<Router>
-				<Header />
-				<Switch>
-					<Route exact path="/" component={Connection} />
-					<Route exact path="/sinscrire" component={Sinscrire} />
-					<Route exact path="/seconnecter" component={SeConnecter} />
-					<Route exact path="/cards" component={CardCompany} />
-					<Route exact path="/match" component={Match} />
-                    <Route exact path ="/profil" component={ProfilPage} />
-				</Switch>
-			</Router>
-		</div>
-	);
+  return (
+    <div className="App">
+      <Reset />
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Connection} />
+          <Route exact path="/sinscrire" component={Sinscrire} />
+          <Route exact path="/seconnecter" component={SeConnecter} />
+          <Route exact path="/cards" component={CardCompany} />
+          <Route exact path="/match" component={Match} />
+          <Route exact path="/profil/:prenom" component={ProfilPage} />
+        </Switch>
+      </Router>
+    </div>
+  );
 }
